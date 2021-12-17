79ffbcc1220537f5a3fc8b99eaf2ec8f08b2ed42
无意白纸 <66541623+WuYiBaiZhi@users.noreply.github.com>
2021-12-17 10:56:46 +0800

    Add files via upload

---
+++ Python笔记/Python/输出和数据类型.py
@@ -0,0 +1,32 @@
+# _*_ coding:UTF-8 _*_
+# Time : 2021/12/17 8:11
+# Auth : 无意白纸
+# File : 编写第一个Python程序.py
+# IDE  : PyCharm
+
+'''
+#向控制台打印一个Hello Word
+print("Hello World")    #这是我的第一个Python程序
+print('这是我的第一个Python程序')
+print('标准化输出字符串')
+#给a赋值
+a = 10
+print('这是变量a:',a)
+'''
+'''
+#格式化输出
+age = 10
+print("我今年%d"%age)
+age += 1
+print("我今年%d"%age)
+age += 1
+print("我今年%d"%age)
+print("我的名字是%s,我的国籍是%s" %("无意白纸","中国"))
+print('www','baidu','com',sep='.')
+print('Hello',end='')
+print('World',end='\t')
+print('python',end='\n')
+print('end')
+'''
+print('12345678-------')
+print('12345678\n------')
